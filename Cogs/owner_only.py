8954b21d7e1e762eb47d391403b0ba4026047702
Mahasvan <mahasvan@gmail.com>
2021-07-24 14:25:38 +0530

    added commands to load and unload cogs

--- Cogs/owner_only.py
+++ Cogs/owner_only.py
@@ -32,8 +32,8 @@ class OwnerOnly(commands.Cog, description='A bunch of owner-only commands.\n'
                                                  "This is Owner-only, so don't have any funny ideas.", )
     @commands.is_owner()
     async def reload(self, ctx, cog=None):
-        if not cog:
-            async with ctx.typing():
+        async with ctx.typing():
+            if not cog:
                 embed = discord.Embed(title="Reloading cogs!", color=discord.Color.random(),
                                       timestamp=ctx.message.created_at)
                 for ext in os.listdir("./Cogs/"):
@@ -45,29 +45,59 @@ class OwnerOnly(commands.Cog, description='A bunch of owner-only commands.\n'
                                 name=f"Reloaded: `{ext}`", value='\uFEFF', inline=False)
                         except Exception as e:
                             embed.add_field(
-                                name=f"Failed to reload: `{ext}`", value=e, inline=False)
+                                name=f"Failed to reload: `{ext}`", value=str(e), inline=False)
                         await asyncio.sleep(0.5)
                 await ctx.send(embed=embed)
-        else:
-            async with ctx.typing():
-                embed = discord.Embed(
-                    title="Reloading cogs!", color=discord.Color.random(), timestamp=ctx.message.created_at)
-                ext = f"{cog.lower()}.py"
-                if not os.path.exists(f"./cogs/{ext}"):
-                    embed.add_field(
-                        name=f"Failed to reload: `{ext}`", value="This cog does not exist.", inline=False)
+                return
 
-                elif ext.endswith(".py") and not ext.startswith("_"):
-                    try:
-                        self.bot.unload_extension(f"Cogs.{ext[:-3]}")
-                        self.bot.load_extension(f"Cogs.{ext[:-3]}")
-                        embed.add_field(
-                            name=f"Reloaded: `{ext}`", value='\uFEFF', inline=False)
-                    except Exception:
-                        desired_trace = traceback.format_exc()
-                        embed.add_field(
-                            name=f"Failed to reload: `{ext}`", value=desired_trace, inline=False)
-                await ctx.send(embed=embed)
+            """ Now the code for reloading One cog comes into play"""
+
+            embed = discord.Embed(
+                title="Reloading cogs!", color=discord.Color.random(), timestamp=ctx.message.created_at)
+            ext = f"{cog.lower()}.py"
+            if not os.path.exists(f"./cogs/{ext}"):
+                embed.add_field(
+                    name=f"Failed to reload: `{ext}`", value="This cog does not exist.", inline=False)
+
+            elif ext.endswith(".py") and not ext.startswith("_"):
+                try:
+                    self.bot.unload_extension(f"Cogs.{ext[:-3]}")
+                    self.bot.load_extension(f"Cogs.{ext[:-3]}")
+                    embed.add_field(
+                        name=f"Reloaded: `{ext}`", value='\uFEFF', inline=False)
+                except Exception:
+                    desired_trace = traceback.format_exc()
+                    embed.add_field(
+                        name=f"Failed to reload: `{ext}`", value=desired_trace, inline=False)
+            await ctx.send(embed=embed)
+
+    @commands.command(name='load', description='Loads a Cog.')
+    async def load_cog(self, ctx, cog_file_name):
+        embed = discord.Embed(title=f"Loading Cog {cog_file_name}.py!", color=discord.Color.random(),
+                              timestamp=ctx.message.created_at)
+        if os.path.exists(f"./Cogs/{cog_file_name}.py"):
+            try:
+                self.bot.load_extension(f"Cogs.{cog_file_name}")
+                embed.add_field(
+                    name=f"Loaded: `{cog_file_name}.py`", value='\uFEFF', inline=False)
+            except Exception as e:
+                embed.add_field(
+                    name=f"Failed to load: `{cog_file_name}.py`", value=str(e), inline=False)
+            await ctx.send(embed=embed)
+
+    @commands.command(name='unload')
+    async def unload_cog(self, ctx, cog_file_name):
+        embed = discord.Embed(title=f"Unloading Cog {cog_file_name}.py!", color=discord.Color.random(),
+                              timestamp=ctx.message.created_at)
+        if os.path.exists(f"./Cogs/{cog_file_name}.py"):
+            try:
+                self.bot.unload_extension(f"Cogs.{cog_file_name}")
+                embed.add_field(
+                    name=f"Unloaded: `{cog_file_name}.py`", value='\uFEFF', inline=False)
+            except Exception as e:
+                embed.add_field(
+                    name=f"Failed to unload: `{cog_file_name}.py`", value=str(e), inline=False)
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
