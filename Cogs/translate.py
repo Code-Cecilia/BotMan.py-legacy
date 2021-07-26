cedbe709deac26aa0b5275b02d1e55cfa44a1cf6
Mahasvan <mahasvan@gmail.com>
2021-07-26 11:38:10 +0530

    more optimized code for translate. fixed langcodes with len >2 not translating

--- Cogs/translate.py
+++ Cogs/translate.py
@@ -15,6 +15,7 @@ class Translate(commands.Cog, description='A set of commands that uses the googl
         self.translator = Translator()
         self.lang_dict = googletrans.LANGUAGES
         self.lang_code_dict = googletrans.LANGCODES
+        self.lang_list = list(googletrans.LANGUAGES.keys())
 
     @commands.command(name='translate', aliases=['tr'], description='Translate from one language to the other.\n'
                                                                     'Usage: `bm-translate Hola es en` --> '
@@ -22,31 +23,31 @@ class Translate(commands.Cog, description='A set of commands that uses the googl
                                                                     '`bm-translate Hola en` --> '
                                                                     'translates by detecting the language')
     async def translate(self, ctx, *, text):
-        split_text = text.split(' ')
-        dest_lang = split_text[-1]
+        split_text = text.split()
         src_lang = split_text[-2]
-        if len(src_lang) != 2:
-            src_lang = None
+        dest_lang = split_text[-1]
 
-        if src_lang is not None:
-            split_text.pop(-1)  # removing the to_language entry
-            split_text.pop(-1)  # removing the from_language entry
-            to_translate = " ".join(split_text)
-            try:
-                result = self.translator.translate(to_translate, src=src_lang, dest=dest_lang)
-            except ValueError:
-                return await ctx.send('Improper format. Check the help command for the correct format(s).')
+        if str(src_lang) not in self.lang_list:
+            has_src = True
         else:
-            split_text.pop(-1)
-            to_translate = " ".join(split_text)
-            try:
-                result = self.translator.translate(to_translate, dest=dest_lang)
-            except ValueError:
-                return await ctx.send('Improper format. Check the help command for the correct format(s).')
-        from_lang = str(self.lang_dict.get(result.src)).title()
-        dest_lang = str(self.lang_dict.get(result.dest)).title()
-        final_string = f"{from_lang} --> {dest_lang}"
+            has_src = False
+        if dest_lang not in self.lang_list:
+            return print(f'Could not find language {dest_lang}.')
 
+        split_text.pop(-1)  # remove destination lanf
+        if not has_src:  # remove source lang if exists
+            split_text.pop(-1)
+
+        to_translate = " ".join(split_text)
+
+        if not has_src:
+            result = self.translator.translate(to_translate, src=src_lang, dest=dest_lang)
+        else:
+            result = self.translator.translate(to_translate, dest=dest_lang)
+
+        from_lang = str(self.lang_dict.get(str(result.src).lower())).title()
+        to_lang = str(self.lang_dict.get(str(result.dest)).lower()).title()
+        final_string = f"{from_lang} --> {to_lang}"
         embed = discord.Embed(title=f"{ctx.author.display_name}, your translation is:", color=discord.Color.random())
         embed.description = result.text
         embed.set_footer(text=f"{final_string} | Powered by Google Translate")
