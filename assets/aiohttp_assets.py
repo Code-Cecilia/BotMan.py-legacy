2be38337ef8d0c1afda265d4348cd32af13ad0c6
Mahas1 <mahasvan@gmail.com>
2021-12-07 19:00:16 +0530

    meme commands!

--- assets/aiohttp_assets.py
+++ assets/aiohttp_assets.py
@@ -13,3 +13,17 @@ async def aiohttp_get_binary(url: str):
         async with session.get(url) as response:
             response = (await response.content.read())
             return response
+
+
+async def aiohttp_post(url: str, data: dict = None, params: dict = None):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data, params=params) as response:
+            response = (await response.content.read()).decode('utf-8')
+            return response
+
+
+async def aiohttp_post_binary(url: str, data: dict = None, params: dict = None):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data, params=params) as response:
+            response = (await response.content.read())
+            return response
